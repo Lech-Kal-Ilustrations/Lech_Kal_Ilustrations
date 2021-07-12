5d5b211897a303c6e698c6995d51b5e43c6118e1
Lech-Kal-Ilustrations <87287481+Lech-Kal-Ilustrations@users.noreply.github.com>
2021-07-11 22:11:56 -0400

    Add files via upload

---
+++ js/nabvar.js
@@ -0,0 +1,17 @@
+const hamBurgern = document.querySelector(".ham-burger");
+
+hamBurgern.addEventListener("click", function(){
+
+    document.getElementById('nabvar').style.display = 'block';;
+    document.getElementById('ham-burger').style.display = 'none';;
+    document.getElementById('ham-burger1').style.display = 'block';;
+});
+
+const hamBurgern1 = document.querySelector(".ham-burger1");
+
+hamBurgern1.addEventListener("click", function(){
+
+    document.getElementById('nabvar').style.display = 'none';;
+    document.getElementById('ham-burger').style.display = 'block';;
+    document.getElementById('ham-burger1').style.display = 'none';;
+});
