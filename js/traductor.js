29742bed2f6c2497d6645cc48ad31e15728ddcb8
Lech-Kal-Ilustrations <87287481+Lech-Kal-Ilustrations@users.noreply.github.com>
2021-08-05 22:49:17 -0400

    Add files via upload

---
+++ js/traductor.js
@@ -0,0 +1,119 @@
+const traductorIngles = document.querySelector(".ingles");
+
+traductorIngles.addEventListener("click", function(){
+
+    document.getElementById('1ingles').style.display = 'block';;
+    document.getElementById('2ingles').style.display = 'block';;
+    document.getElementById('3ingles').style.display = 'block';;
+    document.getElementById('4ingles').style.display = 'block';;
+    document.getElementById('5ingles').style.display = 'block';;
+
+    document.getElementById('1español').style.display = 'none';;
+    document.getElementById('2español').style.display = 'none';;
+    document.getElementById('3español').style.display = 'none';;
+    document.getElementById('4español').style.display = 'none';;
+    document.getElementById('5español').style.display = 'none';;
+
+    document.getElementById('1frances').style.display = 'none';;
+    document.getElementById('2frances').style.display = 'none';;
+    document.getElementById('3frances').style.display = 'none';;
+    document.getElementById('4frances').style.display = 'none';;
+    document.getElementById('5frances').style.display = 'none';;
+
+    document.getElementById('1italiano').style.display = 'none';;
+    document.getElementById('2italiano').style.display = 'none';;
+    document.getElementById('3italiano').style.display = 'none';;
+    document.getElementById('4italiano').style.display = 'none';;
+    document.getElementById('5italiano').style.display = 'none';;
+
+});
+
+const traductorFrances = document.querySelector(".frances");
+
+traductorFrances.addEventListener("click", function(){
+
+    document.getElementById('1frances').style.display = 'block';;
+    document.getElementById('2frances').style.display = 'block';;
+    document.getElementById('3frances').style.display = 'block';;
+    document.getElementById('4frances').style.display = 'block';;
+    document.getElementById('5frances').style.display = 'block';;
+
+    document.getElementById('1español').style.display = 'none';;
+    document.getElementById('2español').style.display = 'none';;
+    document.getElementById('3español').style.display = 'none';;
+    document.getElementById('4español').style.display = 'none';;
+    document.getElementById('5español').style.display = 'none';;
+
+    document.getElementById('1ingles').style.display = 'none';;
+    document.getElementById('2ingles').style.display = 'none';;
+    document.getElementById('3ingles').style.display = 'none';;
+    document.getElementById('4ingles').style.display = 'none';;
+    document.getElementById('5ingles').style.display = 'none';;
+
+    document.getElementById('1italiano').style.display = 'none';;
+    document.getElementById('2italiano').style.display = 'none';;
+    document.getElementById('3italiano').style.display = 'none';;
+    document.getElementById('4italiano').style.display = 'none';;
+    document.getElementById('5italiano').style.display = 'none';;
+
+});
+
+const traductorItaliano = document.querySelector(".italiano");
+
+traductorItaliano.addEventListener("click", function(){
+
+    document.getElementById('1italiano').style.display = 'block';;
+    document.getElementById('2italiano').style.display = 'block';;
+    document.getElementById('3italiano').style.display = 'block';;
+    document.getElementById('4italiano').style.display = 'block';;
+    document.getElementById('5italiano').style.display = 'block';;
+
+    document.getElementById('1español').style.display = 'none';;
+    document.getElementById('2español').style.display = 'none';;
+    document.getElementById('3español').style.display = 'none';;
+    document.getElementById('4español').style.display = 'none';;
+    document.getElementById('5español').style.display = 'none';;
+
+    document.getElementById('1ingles').style.display = 'none';;
+    document.getElementById('2ingles').style.display = 'none';;
+    document.getElementById('3ingles').style.display = 'none';;
+    document.getElementById('4ingles').style.display = 'none';;
+    document.getElementById('5ingles').style.display = 'none';;
+
+    document.getElementById('1frances').style.display = 'none';;
+    document.getElementById('2frances').style.display = 'none';;
+    document.getElementById('3frances').style.display = 'none';;
+    document.getElementById('4frances').style.display = 'none';;
+    document.getElementById('5frances').style.display = 'none';;
+
+});
+
+const traductorEspañol = document.querySelector(".español");
+
+traductorEspañol.addEventListener("click", function(){
+
+    document.getElementById('1español').style.display = 'block';;
+    document.getElementById('2español').style.display = 'block';;
+    document.getElementById('3español').style.display = 'block';;
+    document.getElementById('4español').style.display = 'block';;
+    document.getElementById('5español').style.display = 'block';;
+
+    document.getElementById('1ingles').style.display = 'none';;
+    document.getElementById('2ingles').style.display = 'none';;
+    document.getElementById('3ingles').style.display = 'none';;
+    document.getElementById('4ingles').style.display = 'none';;
+    document.getElementById('5ingles').style.display = 'none';;
+
+    document.getElementById('1frances').style.display = 'none';;
+    document.getElementById('2frances').style.display = 'none';;
+    document.getElementById('3frances').style.display = 'none';;
+    document.getElementById('4frances').style.display = 'none';;
+    document.getElementById('5frances').style.display = 'none';;
+
+    document.getElementById('1italiano').style.display = 'none';;
+    document.getElementById('2italiano').style.display = 'none';;
+    document.getElementById('3italiano').style.display = 'none';;
+    document.getElementById('4italiano').style.display = 'none';;
+    document.getElementById('5italiano').style.display = 'none';;
+
+});
